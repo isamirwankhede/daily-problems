3f42c9c652c0e653ba798b7c71ed97203de4ee2e
Samir Wankhede <contactsamirw@gmail.com>
2026-07-23 09:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (97.22%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -6,13 +6,13 @@ class Solution {
         int right = n-1;
 
         while(left < right){
-            int mid = left + (right - left)/2;
+            int mid = left + (right - left)/2+1;
 
-            if(nums[mid] > nums[mid+1]){
-                right = mid;
+            if(nums[mid] > nums[mid-1]){
+                left = mid;
 
             }else{
-                left = mid +1;
+                right = mid -1;
             }
         }
 
