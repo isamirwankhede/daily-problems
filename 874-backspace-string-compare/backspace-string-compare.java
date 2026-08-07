5d1c572085c2a71d6ea2e2e83a1b1863d844bef6
Samir Wankhede <contactsamirw@gmail.com>
2026-08-07 11:31:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (88.80%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,28 +1,54 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        return build(s).equals(build(t));
-    }
+        int i = s.length() - 1;
+        int j = t.length() - 1;
 
-    private String build(String s) {
-        Stack<Character> st = new Stack<>();
+        int skipS = 0;
+        int skipT = 0;
 
-        for (char ch : s.toCharArray()) {
-            if (ch == '#') {
-                if (!st.isEmpty()) {
-                    st.pop();
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (s.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+
+                } else {
+                    break;
                 }
             }
-             else {
-                    st.push(ch);
+            while (j >= 0) {
+                if (t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+
+                } else {
+                    break;
                 }
+            }
+
+            if (i >= 0 && j >= 0) {
+                if (s.charAt(i) != t.charAt(j))
+                    return false;
+            } else {
+
+                if (i >= 0 || j >= 0)
+                    return false;
+            }
+            i--;
+            j--;
+
         }
 
-        StringBuilder ans = new StringBuilder();
-        for (char c : st) {
-            ans.append(c);
-        }
-
-        return ans.toString();
+        return true;
 
     }
+
 }
